47a41d34aa8d297f338447daee2ae71ea2d09bbd
vincentbollaert <vincentbollaert@gmail.com>
2025-02-12 22:20:07 +0100

    init commit /w changes

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,16 @@
-import { prisma } from "@repo/database";
+import { prisma } from '@repo/database'
+import { Card } from '@repo/ui/Card'
 
 export default async function IndexPage() {
-  const users = await prisma.user.findMany();
+  const users = await prisma.user.findMany()
 
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello World 2</h1>
       <pre>{JSON.stringify(users, null, 2)}</pre>
+      <Card title="test title" href="">
+        Test description
+      </Card>
     </div>
-  );
+  )
 }
