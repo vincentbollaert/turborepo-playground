6e964e3175b44db7a5315c324aba72bde3eec6b3
vincentbollaert <vincentbollaert@gmail.com>
2025-02-25 10:12:12 +0100

    misc small fixes

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -3,13 +3,14 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactNode } from 'react'
 
-// Create a client
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+})
 
 export function Providers({ children }: { children: ReactNode }) {
-  return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-    </QueryClientProvider>
-  )
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 }
