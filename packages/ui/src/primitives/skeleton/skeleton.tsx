992ad7073f05b58f2e14fdb9f4682899ba47b72d
vincentbollaert <vincentbollaert@gmail.com>
2025-03-02 16:37:12 +0100

    add testid to skeleton

--- packages/ui/src/primitives/skeleton/skeleton.tsx
+++ packages/ui/src/primitives/skeleton/skeleton.tsx
@@ -2,5 +2,5 @@ import { cn } from '../../lib/utils'
 import styles from './skeleton.module.scss'
 
 export const Skeleton = ({ className, ...props }: React.ComponentProps<'div'>) => {
-  return <div data-slot="skeleton" className={cn(styles.skeleton, className)} {...props} />
+  return <div data-slot="skeleton" className={cn(styles.skeleton, className)} {...props} data-testid="skeleton" />
 }
