0f7e4ee7c61dc1569f25d6b9c118608935c1fe1d
vincentbollaert <vincentbollaert@gmail.com>
2025-03-02 18:14:02 +0100

    add env vars basic impl

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -6,8 +6,12 @@ import { reactQueryClient } from '@repo/api/reactQueryClient'
 import { Layout } from './layout'
 import '@repo/ui/global.scss'
 
+const mode = import.meta.env.MODE
+const devDbUrl = import.meta.env.VITE_LOCAL_DATABASE_URL
+const prodDbUrl = import.meta.env.VITE_PROD_DATABASE_URL
+
 client.setConfig({
-  baseUrl: 'http://localhost:5173/api/v1',
+  baseUrl: mode === 'production' ? prodDbUrl : devDbUrl,
 })
 
 createRoot(document.getElementById('root')!).render(
