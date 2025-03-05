fa1d92877984100f50bbbf095660b6b7807c6997
vincentbollaert <vincentbollaert@gmail.com>
2025-03-05 21:44:31 +0100

    fix lint errors

---
+++ apps/client/vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+import { mergeViteConfig } from '@repo/vitest-config/configs';
+export default defineConfig(() => mergeViteConfig({
+    test: {
+        setupFiles: ['./setupTests.ts'],
+    }
+}));
--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import { mergeViteConfig } from '@repo/vitest-config/configs'
 
-export default defineConfig((env) => 
+export default defineConfig(() => 
   mergeViteConfig({
     test: {
       setupFiles: ['./setupTests.ts'],
