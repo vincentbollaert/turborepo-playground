93ff2cbba4e5754602fc998e79454dbf31fcc8b0
vincentbollaert <vincentbollaert@gmail.com>
2025-03-17 20:49:56 +0100

    fix nextjs window access

--- apps/client-next/app/page.tsx
+++ apps/client-next/app/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 
-import { AppSwitcher } from "@repo/ui/appSwitcher";
+// import { AppSwitcher } from "@repo/ui/appSwitcher";
 import { Frame } from "@repo/ui/frame";
 import { useMswInit } from "@repo/ui/hooks";
 import { MockSelector } from "@repo/ui/mockSelector";
 import { Socials } from "@repo/ui/socials";
 import { Home } from "./home";
+
+import dynamic from "next/dynamic";
+
+const AppSwitcher = dynamic(() => import("@repo/ui/appSwitcher").then((mod) => mod.AppSwitcher), {
+  ssr: false,
+});
 
 export default function IndexPage() {
   const mswStatus = useMswInit();
